9953f1d5c3ba384b96f5f5abfaa3141c7071f62f
Bruno Santimaria <brun0.23@outlook.com>
2023-09-19 12:06:21 -0300

    Agregado Theme provider

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,12 @@
 import { AppRouter } from "./router/AppRouter";
+import { AppTheme } from "./theme";
 
 export const JournalApp = () => {
   return (
     <>
-      <AppRouter />
+      <AppTheme>
+        <AppRouter />
+      </AppTheme>
     </>
   );
 };
