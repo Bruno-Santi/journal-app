1f061a88ecd87a610a0e629a163f066a64af0b68
Bruno Santimaria <brun0.23@outlook.com>
2023-09-19 11:40:07 -0300

    Creada estructura del proyecto

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path='auth/*' element={<AuthRoutes />} />
+
+      <Route path='/*' element={<JournalRoutes />} />
+    </Routes>
+  );
+};
